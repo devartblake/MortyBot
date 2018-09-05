3790a684e90741bd2b8c3799ce3f41180fd2a73e
devartblake <devart.blake@gmail.com>
2018-09-05 05:27:37 -0400

    Initial commit

---
+++ app/renderer/components/Sidebar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="l-sidebar">
+    <profile v-for="(p, index) in profiles" :data="p" :index="index" :key="index"></profile>
+    <action icon="user-plus" :func="addProfile"></action>
+    <action icon="plus-circle" :func="addColumn"></action>
+  </div>
+</template>
+
+<script>
+import Action from './Sidebar/Action'
+import Profile from './Sidebar/Profile'
+import { mapGetters } from 'vuex'
+export default {
+  computed: mapGetters({
+    profiles: 'allProfiles'
+  }),
+  methods: {
+    addColumn (e) {
+      this.$modal.show('column-modal')
+    },
+    addProfile (e) {
+      this.$modal.show('profile-modal')
+    }
+  },
+  components: {
+    Action,
+    Profile
+  },
+  name: 'sidebar'
+}
+</script>
+
+<style>
+.l-sidebar {
+  background-color: #202020;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 50px;
+  z-index: 200;
+}
+</style>
---
+++ app/renderer/components/sidebar/Action.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="c-sidebar__button" @click="func()">
+      <icon :name="icon"></icon>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['icon', 'func']
+}
+</script>
+
+<style>
+.c-sidebar__button {
+  padding: 0 10px;
+  margin-top: 10px;
+  color: #b0bec5;
+  text-align: center;
+  user-select: none;
+  cursor: pointer;
+}
+</style>
